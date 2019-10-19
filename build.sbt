2e2e6d7a92a134377c1b62e0e5d35377cdcb50a8
Brian Topping <brian@coglative.com>
2019-10-19 16:19:12 -0600

    kdtree test

--- build.sbt
+++ build.sbt
@@ -8,11 +8,14 @@ lazy val cassandraPluginVersion = "0.99"
 
 fork in Test := true
 
+resolvers += "Sonatype OSS Releases" at "http://oss.sonatype.org/content/repositories/releases/"
+
 libraryDependencies ++= Seq(
     "com.typesafe.akka" %% "akka-cluster" % akkaVersion,
     "com.typesafe.akka" %% "akka-cluster-tools" % akkaVersion,
     "com.typesafe.akka" %% "akka-slf4j" % akkaVersion,
     "ch.qos.logback" % "logback-classic" % "1.2.3",
+    "com.thesamet" %% "kdtree" % "1.0.4",
     "com.github.scopt" %% "scopt" % "3.7.1",
 
     // test dependencies
--- src/main/scala/boundingbox/Main.scala
+++ src/main/scala/boundingbox/Main.scala
@@ -1,8 +1,37 @@
 package boundingbox
 
+import com.thesamet.spatial.{DimensionalOrdering, KDTree, Metric, Region}
+
 import scala.io.Source
 
+
 object Main {
+    implicit val dimOrdering: DimensionalOrdering[Point] =
+        new DimensionalOrdering[Point] {
+            override def dimensions: Int = 2
+            override def compareProjection(dimension: Int)(x: Point, y: Point): Int = {
+                dimension match {
+                    case 0 => x.x - y.x
+                    case 1 => x.y - y.y
+                }
+            }
+        }
+
+    implicit val metric: Metric[Point, Int] = new Metric[Point, Int] {
+        /** Returns the distance between two points. */
+        override def distance(x: Point, y: Point): Int = x.x * y.x + x.y * y.y
+
+        /** Returns the distance between x and a hyperplane that passes through y and perpendicular to
+         * that dimension.
+         */
+        override def planarDistance(dimension: Int)(x: Point, y: Point): Int = {
+            dimension match {
+                case 0 => x.x * y.x
+                case 1 => x.y * y.y
+            }
+        }
+    }
+
     def main(args: Array[String]): Unit = {
         args.headOption match {
             case None =>
@@ -25,6 +54,20 @@ object Main {
                     case Nil =>
                         println("input has no points")
                     case head :: tail =>
+                        val tree    = KDTree.fromSeq(points)
+                        println(tree)
+                        //                        val tl      = Point(2, 10)
+//                        val br      = Point(4, 12)
+//                        val build   = Region.from(tl, 0)
+//                            .and(Region.from(tl, 1))
+//                            .and(Region.to(br, 0))
+//                            .and(Region.to(br, 1))
+//                            .build
+//                        val nearest = tree.regionQuery(build)
+                        val point   = Point(10,2)
+                        val nearest = tree.findNearest(point, 4)
+                        val contiguous = nearest.filter(_.contiguous(point))
+                        println(contiguous)
                         tail.foldLeft(Tree(Rect(head))) {
                             (t, p) => t.merge(Rect(p))
                         }.print()
